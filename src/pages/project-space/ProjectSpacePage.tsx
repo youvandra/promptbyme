a3033c05a88b5379a900fd1565e67b31ba2ae690
Youvandra Febrial <40780823+youvandra@users.noreply.github.com>
2025-06-23 18:26:53 +0700

    Updated ProjectSpacePage.tsx

--- src/pages/project-space/ProjectSpacePage.tsx
+++ src/pages/project-space/ProjectSpacePage.tsx
@@ -587,7 +587,6 @@ useEffect(() => {
                       {selectedProject && (
                         <TeamMembersDisplay 
                           onClick={() => setShowMembersModal(true)}
-                          onClick={() => setShowMembersModal(true)}
                           projectId={selectedProject.id}
                           currentUserRole={currentUserRole}
                         />
